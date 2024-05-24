462b5d11220f48fc9672c7f6251af09fea799ebe
Matei Cloteaux <59572450+mateidragony@users.noreply.github.com>
2024-05-24 15:03:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,8 +35,6 @@ session = cnx.session()
 my_df = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"), col("SEARCH_ON"))
 
 pd_df = my_df.to_pandas()
-st.dataframe(pd_df)
-st.stop()
 
 
 name = st.text_input("Name on Smoothie", "")
@@ -54,8 +52,11 @@ if options:
     ingredients_string = ''
     for fruit in options:
         ingredients_string += fruit + " "
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        
         st.subheader(fruit + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit.lower())
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on.lower())
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
