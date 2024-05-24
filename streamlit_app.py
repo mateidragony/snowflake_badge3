9f1b4fed5b2e12a7a2149969869ea0687bc2ecfc
Matei Cloteaux <59572450+mateidragony@users.noreply.github.com>
2024-05-24 14:24:11 -0400

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,67 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.functions import col, when_matched
+
+
+from snowflake.snowpark.context import get_active_session
+
+# Write directly to the app
+st.title(":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
+st.write(
+    """Orders that need to be filled.
+    """
+)
+
+# option = st.selectbox(
+#    "What is your favorite fruit?",
+#    ("-- Please select a fruit --", "Banana", "Strawberries", "Peaches"),
+# )
+
+# st.write("You selected: ", "None" if option == "-- Please select a fruit --" else option)
+
+
+session = get_active_session()
+my_df = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
+
+if not my_df:
+    st.write("Looks like there's nothing here...SMOOTHIES.PUBLIC."LQ51H1ZBF6_9EJLE (Stage)"")
+else:
+    e_df = st.data_editor(my_df)
+    
+    submitted = st.button("Submit")
+    
+    if submitted:
+        with st.spinner('Updating Dataset..'):
+            original = session.table("smoothies.public.orders")
+            edited = session.create_dataframe(e_df)
+            try:
+                original.merge(edited, 
+                           (original['order_uid'] == edited['order_uid']),
+                          [when_matched().update({'ORDER_FILLED' : edited['ORDER_FILLED']})])
+                st.success("Dataset Updated.")
+            except:
+                st.error("Something went wrong.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
